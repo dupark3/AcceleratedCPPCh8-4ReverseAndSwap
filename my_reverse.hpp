72e13d75310d318258ad9ebaf3e58cbfda5a5666
Du Park <dupark3@gmail.com>
2018-03-02 08:44:26 -0500

    Included swap header and changed function names from swap to my_swap

--- my_reverse.hpp
+++ my_reverse.hpp
@@ -1,11 +1,11 @@
 
-//#include "swap.h"
+#include "my_swap.h"
 
 template <class Bi>
 void my_reverse(Bi first, Bi last){
     while (first != last){
         --last;
         if (first != last)
-            swap(*first++ = *last);
+            my_swap(*first++, *last);
     }
 }
