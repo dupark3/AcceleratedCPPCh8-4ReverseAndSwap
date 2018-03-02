f381365c4c73f9f078943bd100c77ddefbea9596
Du Park <dupark3@gmail.com>
2018-03-02 08:44:42 -0500

    Swap function and header implemented

---
+++ my_swap.h
@@ -0,0 +1,10 @@
+#ifndef GUARD_my_swap_h
+#define GUARD_my_swap_h
+
+#include "my_swap.hpp"
+
+template <class T>
+void my_swap(T&, T&);
+
+
+#endif // GUARD_my_swap_h
---
+++ my_swap.hpp
@@ -0,0 +1,8 @@
+
+
+template <class T>
+void my_swap(T& first, T& second){
+    T temp = first;
+    first = second;
+    second = temp;
+}
