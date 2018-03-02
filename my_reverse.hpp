1ccfb7e8891ab7112a45cb4c80355c668675580c
Du Park <dupark3@gmail.com>
2018-03-02 08:48:51 -0500

    Clean up lines

--- my_reverse.hpp
+++ my_reverse.hpp
@@ -5,7 +5,9 @@ template <class Bi>
 void my_reverse(Bi first, Bi last){
     while (first != last){
         --last;
-        if (first != last)
+        if (first != last){
             my_swap(*first++, *last);
+        }
+
     }
 }
